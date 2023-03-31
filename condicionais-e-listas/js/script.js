d9e40bb5cb90b712daea7594c6f515b3d70924b0
Higor Moroni <moroni.higor@gmail.com>
2023-03-30 21:19:14 -0300

    feat: add v-if and v-for exemples

---
+++ condicionais-e-listas/js/script.js
@@ -0,0 +1,11 @@
+new Vue({
+    el: '#app',
+    data() {
+        return {
+            nome: 'Joaquim',
+            logado: false,
+            anonimo: false,
+            cores: ['Vermelho', 'Verde', 'Amarelo', 'Azul', 'Preto']
+        }
+    }
+});
