ca7a3d8525a09c17cb2d2f6d6e81e2e5034d940a
Higor Moroni <moroni.higor@gmail.com>
2023-03-30 21:57:24 -0300

    feat: add more v-for and key exemples

--- condicionais-e-listas/js/script.js
+++ condicionais-e-listas/js/script.js
@@ -5,7 +5,19 @@ new Vue({
             nome: 'Joaquim',
             logado: false,
             anonimo: false,
-            cores: ['Vermelho', 'Verde', 'Amarelo', 'Azul', 'Preto']
+            cores: ['Vermelho', 'Verde', 'Amarelo', 'Azul', 'Preto'],
+            pessoas: [
+                {
+                    nome: 'Ana',
+                    idade: 26,
+                    peso: 59
+                },
+                {
+                    nome: 'Guilherme',
+                    idade: 22,
+                    peso: 89
+                },
+            ]
         }
     }
 });
