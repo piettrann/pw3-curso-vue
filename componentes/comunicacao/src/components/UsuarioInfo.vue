894bcf2ca4255f7bc756435f7e664bcd16c1add3
Higor Moroni <moroni.higor@gmail.com>
2023-04-27 22:42:57 -0300

    feat: create all component comunication
    Direta,
    Indireta,
    Entre Irmãos
    Event Bus (barramento)

--- componentes/comunicacao/src/components/UsuarioInfo.vue
+++ componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -2,12 +2,52 @@
     <div class="componente">
         <h2>As Informações de Usuário</h2>
         <p>Vários detalhes...</p>
+        <p>Nome: {{ nomeInvertido }}</p>
+        <p>Idade: {{ idade }}</p>
+        <button @click="trocarNome">Trocar Nome</button>
+        <button @click="trocarNomeCB">Trocar Nome Com Callback</button>
     </div>
 </template>
 
 <script>
+import barramento from '../barramento';
+
 export default {
-    
+    // props: ['nome']
+    props: {
+        //nome: String
+        nome: {
+            type: String,
+            // type: String|Number|Object|Function,
+            // required: true,
+            default: 'Antonio',
+            // default: function () {
+            //     return 'Bruno'
+            // }
+        },
+        trocarNomeCB: {
+            type: Function,
+            required: true
+        }
+    },
+    data() {
+        return {
+            idade: 0
+        }
+    },
+    created() {
+        barramento.quandoIdadeMudar(idade => this.idade = idade);
+    },
+    computed: {
+        nomeInvertido() {
+            return this.nome.split('').reverse().join('');
+        }
+    },
+    methods: {
+        trocarNome() {
+            this.$emit('nomeMudou', 'Renata');
+        }
+    }
 }
 </script>
 
