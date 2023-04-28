6630811b13cc37130ece15b91df5d160b8b31418
Higor Moroni <moroni.higor@gmail.com>
2023-04-27 21:10:53 -0300

    feat: create and use first component

---
+++ componentes/introducao/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <AppContador v-for="c in 10" :key="c" />
+        <button>???</button>
+    </div>
+</template>
+
+<script>
+import AppContador from '@/components/AppContador'
+
+export default {
+    components: {
+        AppContador
+    }
+}
+</script>
---
+++ componentes/introducao/src/components/AppContador.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="contador">
+        <span>{{ contador }}</span>
+        <button @click="contador++">+</button>
+        <button @click="contador--">-</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            contador: 0
+        }
+    }
+}
+</script>
+
+<style scoped>
+    span {
+        border-bottom: 1px solid #CCC;
+        height: 30px;
+        padding: 5px 25px;
+    }
+
+    button {
+        height: 30px;
+        width: 30px;
+        border-radius: 50%;
+        background-color: coral;
+        color: white;
+        outline: none;
+    }
+</style>
